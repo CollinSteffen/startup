10ba9b262372a9f9cee499cd9d9dbdb4b205e39f
Collin Steffen <156038315+CollinSteffen@users.noreply.github.com>
2024-02-25 20:21:56 -0700

    correcting some mistakes

--- src/js/post.js
+++ src/js/post.js
@@ -41,7 +41,7 @@ function readStorage(){
 function createPost() {
     event.preventDefault()
     //TODO: grab the data from the author and put it in a variable
-    const profile;
+    //const profile;
     const desc = document.getElementById('desc').value;// TODO: Pull in form data from DOM
     const img = document.getElementById('img').value;// TODO: Format form data to Task Object
 
@@ -49,53 +49,53 @@ function createPost() {
         alert('Please enter a task...');
         return;
     }
-    const newTask = new Task({author, date:Date.now(), desc, id: Date.now(), img, profile})
-    const existingTasks = readStorage();
-    existingTasks.push(newTask);
-    updateStorage(existingTasks);
-    readTasks();
+    const newTask = new Task({author:"profile", date:Date.now(), desc, id: Date.now(), img, profile})
+    const existingPosts = readStorage();
+    existingPosts.push(newPost);
+    updateStorage(existingPosts);
+    readPosts();
     // TODO: Pull in tasks from local storage and push new one to task list array
     // TODO: Save it to local storage
     // TODO: Update DOM (Call readTasks())
     // Hint - Look at the JavaScript code from lab 1B to see how to extract form data
 }
 
-function readTasks() {
-    const taskList = document.getElementById('taskList');
-    taskList.innerHTML = '';
+function readPosts() {
+    const postList = document.getElementById('post');
+    postList.innerHTML = '';
     
-    const tasks = readStorage();
-    tasks.forEach(task => {
-        taskList.innerHTML += task.toHTML();
+    const posts = readStorage();
+    posts.forEach(post => {
+        postList.innerHTML += post.toHTML();
     });
     // TODO: Clear current tasks to not have duplicates
     // TODO: Pull in tasks from local storage
     // TODO: Parse tasks and Update DOM accordingly
 }
 
-function updateTask(id) {
-    const tasks = readStorage();
-    const taskToUpdate = tasks.find((task) => task.id === id);
-    if (taskToUpdate){
-        taskToUpdate.toggle();
-    }
-    // TODO: Update the task in `tasks` array by flipping it's `done` value
-    // TODO: Save to local storage
-    // TODO: Update DOM (Call readTasks())
-}
+// function updateTask(id) {
+//     const tasks = readStorage();
+//     const taskToUpdate = tasks.find((task) => task.id === id);
+//     if (taskToUpdate){
+//         taskToUpdate.toggle();
+//     }
+//     // TODO: Update the task in `tasks` array by flipping it's `done` value
+//     // TODO: Save to local storage
+//     // TODO: Update DOM (Call readTasks())
+// }
 
-function deleteTask(id) {
-    const tasks = readStorage();
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    updateStorage(updatedTasks);
-    readTasks();
-    // TODO: Delete task from `tasks` array
-    // TODO: Save to local storage
-    // TODO: Make the DOM update
-    // TODO: Update DOM (Call readTasks())
+// function deleteTask(id) {
+//     const tasks = readStorage();
+//     const updatedTasks = tasks.filter((task) => task.id !== id);
+//     updateStorage(updatedTasks);
+//     readTasks();
+//     // TODO: Delete task from `tasks` array
+//     // TODO: Save to local storage
+//     // TODO: Make the DOM update
+//     // TODO: Update DOM (Call readTasks())
 
-}
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
-    readTasks();
+    readPosts();
 })
