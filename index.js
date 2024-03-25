4dff643201c9befd7bd0062df74e845c3fa38e98
CollinSteffen <156038315+CollinSteffen@users.noreply.github.com>
2024-03-25 11:54:05 -0600

    updating with the login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
+const cookieParser = require('cookie-parser');
+const DB = require('./database.js');
 
 const app = express();
 
@@ -9,8 +11,79 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the front-end static content hosting
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the applications static content
 app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
 
 // Set up multer for file uploads
 const upload = multer({
@@ -82,24 +155,32 @@ app.use((_req, res) => {
 //   return posts;
 // }
 
-app.post('/storeCredentials', (req, res) => {
-  const { username, password } = req.body;
-  const credentials = { username, password };
-  const jsonContent = JSON.stringify(credentials);
+// app.post('/storeCredentials', (req, res) => {
+//   const { username, password } = req.body;
+//   const credentials = { username, password };
+//   const jsonContent = JSON.stringify(credentials);
 
-  // Write to the JSON file
-  fs.writeFile('credentials.json', jsonContent, 'utf8', (err) => {
-    if (err) {
-      console.error('An error occurred while writing to file:', err);
-      res.status(500).send('Error storing credentials');
-      return;
-    }
-    console.log('Credentials stored successfully.');
-    res.status(200).send('Credentials stored successfully');
+//   // Write to the JSON file
+//   fs.writeFile('credentials.json', jsonContent, 'utf8', (err) => {
+//     if (err) {
+//       console.error('An error occurred while writing to file:', err);
+//       res.status(500).send('Error storing credentials');
+//       return;
+//     }
+//     console.log('Credentials stored successfully.');
+//     res.status(200).send('Credentials stored successfully');
+//   });
+// });
+
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
   });
-});
-
-
+}
 
 // Start server
 app.listen(port, () => {
